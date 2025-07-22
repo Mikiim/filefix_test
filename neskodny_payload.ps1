5f9720e066dfdd39fc3431916574c274588a900b
Mikiim <miki930@seznam.cz>
2025-07-22 12:09:39 +0200

    Update neskodny_payload.ps1

--- neskodny_payload.ps1
+++ neskodny_payload.ps1
@@ -1,4 +1,2 @@
-# stahne soubor z URL a ulozi ho do TEMP
-Invoke-WebRequest -Uri "https://example.com/test.txt" -OutFile "$env:TEMP\test.txt" 
-# spusti notepad a otevre stazeny soubor
-Start-Process notepad.exe "$env:TEMP\test.txt"
+# Tento příkaz vytvoří na ploše soubor s názvem Test_proveden.txt
+New-Item -Path $env:USERPROFILE\Desktop\Test_proveden.txt -ItemType File
